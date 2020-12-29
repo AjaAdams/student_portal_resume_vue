f3e44b0623f0e04ec2d028cc6c8699f3d480d55e
ryan <ryanandrewbaxter@gmail.com>
2020-12-29 13:18:25 -0600

    Adds vmodels for all student attributes

--- src/views/StudentPortalEdit.vue
+++ src/views/StudentPortalEdit.vue
@@ -1,11 +1,57 @@
 <template>
   <div class="about">
     <h1>This is an EDIT page</h1>
-    <button>Update Personal Info</button>
-    <button>Update Experience</button>
-    <button>Update Education</button>
-    <button>Update Skills</button>
-    <button>Update Capstone</button>
+    <p>First Name:
+    <input type="text" v-model="newFirstName"></p>
+    <p>Last Name:
+    <input type="text" v-model="newLastName"></p>
+    <p>Short Bio:
+    <input type="text" v-model="newShortBio"></p>
+    <p>LinkedIn URL:
+    <input type="text" v-model="newLinkedinUrl"></p>
+    <p>Personal Website URL:
+    <input type="text" v-model="newPersonalWebsite"></p>
+    <p>Online Resume URL:
+    <input type="text" v-model="newOnlineResumeUrl"></p>
+    <p>GitHub URL:
+    <input type="text" v-model="newGithubUrl"></p>
+    <p>Photo URL:
+    <input type="text" v-model="newPhotoUrl"></p>
+    <button v-on:click="updateResumeInformation(student)">Update Personal Info</button>
+    <p>Start Date:
+    <input type="text" v-model="newStartDate"></p>
+    <p>End Date:
+    <input type="text" v-model="newEndDate"></p>
+    <p>Job Title:
+    <input type="text" v-model="newJobTitle"></p>
+    <p>CompanyName:
+    <input type="text" v-model="newCompanyName"></p>
+    <p>Details:
+    <input type="text" v-model="newDetails"></p>
+    <button v-on:click="updateResumeExperience(student)">Update Experience</button>
+    <p>Start Date:
+    <input type="text" v-model="newEdStartDate"></p>
+    <p>End Date:
+    <input type="text" v-model="newEdEndDate"></p>
+    <p>Details:
+    <input type="text" v-model="newEdDetails"></p>
+    <p>Degree:
+    <input type="text" v-model="newEdDegree"></p>
+    <p>University Name:
+    <input type="text" v-model="newEdUniversityName"></p>
+    <button v-on:click="updateResumeEducation(student)">Update Education</button>
+    <p>Skill Name:
+    <input type="text" v-model="newSkillName"></p>
+    <button v-on:click="updateResumeSkills(student)">Update Skills</button>
+    <p>Capstone Name:
+    <input type="text" v-model="newCapName"></p>
+    <p>Capstone Description:
+    <input type="text" v-model="newDescription"></p>
+    <p>Capstone URL:
+    <input type="text" v-model="newUrl"></p>
+    <p>Capstone Screen Shot URL:
+    <input type="text" v-model="newScreenShotUrl"></p>
+    <button v-on:click="updateResumeCapstone(student)">Update Capstone</button>
   </div>
 </template>
 
@@ -15,7 +61,7 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      resume: [],
+      student: {},
       newStartDate: "",
       newEndDate: "",
       newJobTitle: "",
@@ -23,8 +69,8 @@ export default {
       newDetails: "",
       newEdStartDate: "",
       newEdEndDate: "",
-      newDegree: "",
-      newUniversityName: "",
+      newEdDegree: "",
+      newEdUniversityName: "",
       newEdDetails: "",
       newSkillName: "",
       newCapName: "",
@@ -44,14 +90,13 @@ export default {
     };
   },
   created: function() {
-    // need to find a way to get current users info on next line
     axios.get("api/students").then(response => {
-      this.resume = response.data;
-      console.log("Here is the users resume", this.resume);
+      this.student = response.data;
+      console.log("Here is the users stuff", this.student);
     });
   },
   methods: {
-    updateResumeExperience: function(resume) {
+    updateResumeExperience: function(student) {
       var params = {
         start_date: this.newStartDate,
         end_date: this.newEndDate,
@@ -59,42 +104,42 @@ export default {
         company_name: this.newCompanyName,
         details: this.newDetails,
       };
-      axios.patch("api/students/" + resume.id, params).then(response => {
+      axios.patch("api/students/" + student.id, params).then(response => {
         console.log("Experience Updated!", response.data);
       });
     },
-    updateResumeEducation: function(resume) {
+    updateResumeEducation: function(student) {
       var params = {
         start_date: this.newEdStartDate,
         end_date: this.newEdEndDate,
-        degree: this.newDegree,
-        university_name: this.newUniversityName,
+        degree: this.newEdDegree,
+        university_name: this.newEdUniversityName,
         details: this.newEdDetails,
       };
-      axios.patch("api/students/" + resume.id, params).then(response => {
+      axios.patch("api/students/" + student.id, params).then(response => {
         console.log("Education Updated!", response.data);
       });
     },
-    updateResumeSkills: function(resume) {
+    updateResumeSkills: function(student) {
       var params = {
         skill_name: this.newSkillName,
       };
-      axios.patch("api/students/" + resume.id, params).then(response => {
+      axios.patch("api/students/" + student.id, params).then(response => {
         console.log("Skills Updated!", response.data);
       });
     },
-    updateResumeCapstone: function(resume) {
+    updateResumeCapstone: function(student) {
       var params = {
         name: this.newCapName,
         description: this.newDescription,
         url: this.newUrl,
         screenshot_url: this.newScreenShotUrl,
       };
-      axios.patch("api/students/" + resume.id, params).then(response => {
+      axios.patch("api/students/" + student.id, params).then(response => {
         console.log("Capstone Updated!", response.data);
       });
     },
-    updateResumeInformation: function(resume) {
+    updateResumeInformation: function(student) {
       var params = {
         first_name: this.newFirstName,
         last_name: this.newLastName,
@@ -106,7 +151,7 @@ export default {
         github_url: this.newGithubUrl,
         photo_url: this.newPhotoUrl,
       };
-      axios.patch("api/students/" + resume.id, params).then(response => {
+      axios.patch("api/students/" + student.id, params).then(response => {
         console.log("Information Updated!", response.data);
       });
     },
